7eadd99cc25cf2433fc4d2642d0e96080175aa53
datowllol <datowl678@gmail.com>
2021-05-11 00:24:28 +0300

    Version 1.0

---
+++ Project1/Tasks.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project1
+{
+    public class Tasks
+    {
+        public static List<int> Task1Function(List<object> input)
+        {
+
+            for (int i = input.Count-1; i >= 0; --i)
+            {
+                if (input[i] is string)
+                {
+                    input.RemoveAt(i);
+                }
+            }
+            var output = input.Cast<int>().ToList();
+            return output;
+
+        }
+
+        public static char Task2Function(string input)
+        {
+            var output = ' ';
+            for (int i =0; i <= input.Length-1; ++i) {
+                var count = input.Count(x => x == input[i]);
+                if (count == 1)
+                    return input[i];
+            }
+
+            return output;
+
+        }
+
+        public static int Task3Function(int input)
+        {
+            if (input < 0)
+                input = input * (-1);
+            var output = 100;
+            var num=input;
+            while (output > 10)
+            {
+                List<int> listOfDigits = new List<int>();
+                while (num > 0)
+                {
+                    listOfDigits.Add(num % 10);
+                    num = num / 10;
+                }
+                listOfDigits.Reverse();
+                output = listOfDigits.Take(listOfDigits.Count).Sum();
+                num = output;
+            }
+
+            return output;
+        }
+
+        public static int Task4Function(List<int> inputArray, int target)
+        {
+            List<KeyValuePair<int, bool>> dict = new List<KeyValuePair<int, bool>>();
+
+            inputArray.Sort();
+
+            for (int i = 0; i < inputArray.Count; ++i)
+            {
+                dict.Insert(i, new KeyValuePair<int, bool>(inputArray[i], false));
+            }
+            var output = 0;
+            var count = 0;
+
+            for (int i = 0; i < dict.Count; ++i)
+            {
+                Console.WriteLine(dict[i].Key + dict[i].Value.ToString());
+                if (dict[i].Key >= target)
+                {
+                    break;
+                    Console.WriteLine("break");
+                }
+                if (dict[i].Value == false)
+                {
+                    Console.WriteLine(dict[i].Key + dict[i].Value.ToString());
+                    count = dict.Where(temp => temp.Key.Equals(target - dict[i].Key) && temp.Value.Equals(false))
+                    .Select(temp => temp)
+                    .Count();
+                    output += count;
+                    Console.WriteLine(count + "" + output);
+                    var key = dict[i].Key;
+                    dict.Remove(new KeyValuePair<int, bool>(dict[i].Key, false));
+                    dict.Insert(i, new KeyValuePair<int, bool>(key, true));
+                    Console.WriteLine(dict[i].Key + dict[i].Value.ToString());
+                }
+            }
+            Console.WriteLine(output);
+            return output;
+        }
+
+        public static string Task5Function(string input)
+        {
+            string output = "";
+            List<string> guestsSeparated = new List<string>();
+            input = input.ToUpper();
+            var semicolon = input.Length - 1;
+            var colon = input.Length - 1;
+            string surname = "";
+            string name = "";
+            for (int i = (input.Length - 1); i >= 0; --i)
+            {
+                if (input[i].Equals(':'))
+                {
+                    surname = input.Substring(i + 1, semicolon - i - 1);
+                    colon = i;
+
+                }
+                if (input[i].Equals(';'))
+                {
+                    name = input.Substring(i + 1, colon - i - 1);
+                    guestsSeparated.Add("(" + surname + "," + name + ")");
+                    semicolon = i;
+                }
+                if (i == 0)
+                {
+                    name = input.Substring(i, colon - i);
+                    guestsSeparated.Add("(" + surname + "," + name + ")");
+                }
+
+
+            }
+            guestsSeparated.Sort();
+            foreach (string item in guestsSeparated)
+                output += item;
+            return output;
+        }
+
+    }
+}
---
+++ Test1/Tests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using Project1;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test1
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1Funct1()
+        {
+            var input = new List<object>() {1,2,"a","b" };
+            var output = new List<int>() { 1, 2 };
+
+            var result = Tasks.Task1Function(input);
+           if (!(output.All(result.Contains) && output.Count == result.Count))
+                throw new Exception("WRONG");
+        }
+
+        [Test]
+        public void Test2Funct1()
+        {
+            var input = new List<object>() { 1, 2, "a", "b", "aasf", "1", "123", 231};
+            var output = new List<int>() { 1, 2, 231 };
+
+            var result = Tasks.Task1Function(input);
+            if (!(output.All(result.Contains) && output.Count == result.Count))
+                throw new Exception("WRONG");
+        }
+
+        [TestCase("stress", 't')]
+        [TestCase("aabbcc", ' ')]
+        public void Test1Funct2(string input, char output)
+        {
+            var charresult = Tasks.Task2Function(input);
+            Assert.AreEqual(output, charresult, "Returned result is incorrect");
+        }
+
+        [TestCase(16, 7)]
+        [TestCase(132189, 6)]
+        public void Test1Funct3(int input, int output)
+        {
+            var result = Tasks.Task3Function(input);
+            Assert.AreEqual(output, result, "Returned result is incorrect");
+        }
+
+        [Test]
+        public void Test1Funct4()
+        {
+            var input = new List<int>() { 1, 3, 6, 2, 2, 0, 4, 5 };
+            var target = 5;
+            var output = 4;
+
+            var result = Tasks.Task4Function(input,target);
+            Assert.AreEqual(output, result, "Returned result is incorrect");
+        }
+
+        [Test]
+        public void Test2Funct4()
+        {
+            var input = new List<int>() { -5, 11, 6, 0, 0, 0, 1, 1 };
+            var target = 6;
+            var output = 4;
+
+            var result = Tasks.Task4Function(input, target);
+            Assert.AreEqual(output, result, "Returned result is incorrect");
+        }
+
+        [TestCase("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:tornbull;Raphael:Corwill;Alfred:Corwill", "(CORWIL,ALFRED)(CORWILL,FRED)(CORWILL,RAPHAEL)(CORWILL,WILFRED)(TORNBULL,BARNEY)(TORNBULL,BETTY)(TORNBULL,BJON)")]
+        [TestCase("Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Fred:Corwill", "(CORWIL,FRED)(TORNBULL,BARNEY)(TORNBULL,BETTY)(TORNBULL,BJON)")]
+        public void Test1Funct5(string input, string output)
+        {
+            var result = Tasks.Task5Function(input);
+            Assert.AreEqual(output, result, "Returned result is incorrect");
+        }
+
+    }
+}
